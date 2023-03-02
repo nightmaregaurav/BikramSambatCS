7204d2b166ceba4b974bb9cd4f287a4445a084f5
Gaurav Nyaupane <mail.gauravnyaupane@gmail.com>
2023-03-02 14:23:11 +0545

    Added GetMonthEnd

--- DateConverterService.cs
+++ DateConverterService.cs
@@ -14,6 +14,8 @@ namespace DateConverter
 
         public static NepaliDate TodayBs() => DateOnly.FromDateTime(DateTime.Today).ToBs();
 
+        public static NepaliDate GetMonthEndBs(this NepaliDate date) => new(date.Year, date.Month, DateData.DaysInMonthsForBsYear[date.Year][date.Month - 1]);
+        
         public static string ToNepaliNumber(this int number) => number.ToString().ToNepaliNumber();
         internal static string ToNepaliNumber(this string number) =>  number.Replace("0", "०").Replace("1", "१").Replace("2", "२").Replace("3", "३").Replace("4", "४").Replace("5", "५").Replace("6", "६").Replace("7", "७").Replace("8", "८").Replace("9", "९");
 
