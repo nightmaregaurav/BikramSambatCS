2b9196bf5b50e9dc04cc19e3e0170d892e559ca2
Gaurav Nyaupane <mail.gauravnyaupane@gmail.com>
2023-03-02 11:22:05 +0545

    typo

--- Exceptions/DateToConvertCannotComeAfterEndBsDateException.cs
+++ Exceptions/DateToConvertCannotComeAfterEndBsDateException.cs
@@ -2,7 +2,7 @@ namespace DateConverter.Exceptions
 {
     public class DateToConvertCannotComeAfterEndBsDateException : Exception
     {
-        public DateToConvertCannotComeAfterEndBsDateException(int endBsDate) : base(message:$"Date to convert cannot come after {endBsDate}BS")
+        public DateToConvertCannotComeAfterEndBsDateException(int endBsDate) : base(message:$"Date to convert cannot come after {endBsDate}BS.")
         {
         }
     }
