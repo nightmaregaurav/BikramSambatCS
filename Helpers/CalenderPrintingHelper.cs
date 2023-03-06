2dc005ed79313f42300f1b878b395d52d16c0cb9
Gaurav Nyaupane <mail.gauravnyaupane@gmail.com>
2023-03-06 15:04:21 +0545

    Added calenders with ad in nepali

--- Helpers/CalenderPrintingHelper.cs
+++ Helpers/CalenderPrintingHelper.cs
@@ -4,6 +4,8 @@ namespace DateConverter.Helpers
 {
     public abstract class CalenderPrintingHelper
     {
+        private const string AdBsDaySeparator = ":";
+
         public static string GetBsWeekCalender(int year, int month, int referenceDay)
         {
             var date = new NepaliDate(year, month, referenceDay);
@@ -169,7 +171,7 @@ namespace DateConverter.Helpers
             {
                 if (day != null)
                 {
-                    result += day + "/" + startDate.ToAd().Day;
+                    result += day + AdBsDaySeparator + startDate.ToAd().Day;
                     startDate += 1;
                 }
                 result += "\t";
@@ -196,7 +198,7 @@ namespace DateConverter.Helpers
                 {
                     if (day != null)
                     {
-                        result += day + "/" + date.ToAd().Day;
+                        result += day + AdBsDaySeparator + date.ToAd().Day;
                         date += 1;
                     }
                     result += "\t";
@@ -234,7 +236,102 @@ namespace DateConverter.Helpers
                     {
                         if (day != null)
                         {
-                            result += day + "/" + date.ToAd().Day;
+                            result += day + AdBsDaySeparator + date.ToAd().Day;
+                            date += 1;
+                        }
+                        result += "\t";
+                    }
+
+                    result += "\n";
+                }
+            }
+
+            return result;
+        }
+        
+        public static string GetBsWeekCalenderWithAdInNepali(int year, int month, int referenceDay)
+        {
+            var date = new NepaliDate(year, month, referenceDay);
+            var calender = date.GetWeekCalender();
+            var startDate = new NepaliDate(year, month, calender.Min() ?? referenceDay);
+            var endDate = startDate.GetWeekend();
+            var header1 = $"बिसं: {year} {startDate.NepaliMonthName} {calender.Min().ToNepaliNumber()}-{calender.Max().ToNepaliNumber()} सप्ताह\n";
+            var header2 = $"AD: ({startDate.ToAd().Year} {startDate.ToAd().ToString("MMMM")} {startDate.ToAd().Day}) - ({endDate.ToAd().Year} {endDate.ToAd().ToString("MMMM")} {endDate.ToAd().Day}) Week\n";
+            var result = $"{header1}{header2}";
+            result += "-".Multiply(Math.Max(header1.Length, header2.Length)) + "\n";
+            result += DateData.DayNamesInNepaliShortest.Aggregate((s, s1) => s + "\t" + s1).Trim() + "\n";
+            result += "---\t".Multiply(7) + "\n";
+            
+            foreach (var day in calender)
+            {
+                if (day != null)
+                {
+                    result += day.ToNepaliNumber() + AdBsDaySeparator + startDate.ToAd().Day;
+                    startDate += 1;
+                }
+                result += "\t";
+                startDate += 1;
+            }
+
+            return result.Trim();
+        }
+        
+        public static string GetBsMonthCalenderWithAdInNepali(int year, int month)
+        {
+            var date = new NepaliDate(year, month);
+            var calender = date.GetMonthCalender();
+            var header1 = $"बिसं: {year.ToNepaliNumber()} {date.NepaliMonthName} महिना\n";
+            var header2 = $"AD: ({date.ToAd().Year} {date.ToAd().ToString("MMMM")}) - ({date.GetMonthEnd().ToAd().Year} {date.GetMonthEnd().ToAd().ToString("MMMM")}) Month\n";
+            var result = $"{header1}{header2}";
+            result += "-".Multiply(Math.Max(header1.Length, header2.Length)) + "\n";
+            result += DateData.DayNamesInNepaliShortest.Aggregate((s, s1) => s + "\t" + s1).Trim() + "\n";
+            result += "---\t".Multiply(7) + "\n";
+            
+            foreach (var week in calender)
+            {
+                foreach (var day in week)
+                {
+                    if (day != null)
+                    {
+                        result += day.ToNepaliNumber() + AdBsDaySeparator + date.ToAd().Day;
+                        date += 1;
+                    }
+                    result += "\t";
+                }
+
+                result += "\n";
+            }
+
+            return result.Trim();
+        }
+        
+        public static string GetBsYearCalenderWithAdInNepali(int year)
+        {
+            var date = new NepaliDate(year);
+            var calender = date.GetYearCalender();
+            var header1 = $"बिसं: {year.ToNepaliNumber()} बर्ष\n";
+            var header2 = $"AD: {date.ToAd().Year} - {date.GetYearEnd().ToAd().Year} Year\n";
+            var result = $"{header1}{header2}";
+            result += "-".Multiply(Math.Max(header1.Length, header2.Length)) + "\n";
+
+            var monthCounter = 0;
+            foreach (var month in calender)
+            {
+                var monthHeader = $"{DateData.MonthNamesInNepali[monthCounter]} : ({date.ToAd().ToString("MMMM")} - {date.GetMonthEnd().ToAd().ToString("MMMM")})\n";
+                monthCounter++;
+                
+                result += monthHeader;
+                result += "-".Multiply(monthHeader.Length) + "\n";
+                result += DateData.DayNamesInNepaliShortest.Aggregate((s, s1) => s + "\t" + s1).Trim() + "\n";
+                result += "---\t".Multiply(7) + "\n";
+            
+                foreach (var week in month)
+                {
+                    foreach (var day in week)
+                    {
+                        if (day != null)
+                        {
+                            result += day.ToNepaliNumber() + AdBsDaySeparator + date.ToAd().Day;
                             date += 1;
                         }
                         result += "\t";
--- Helpers/NumberConversionHelper.cs
+++ Helpers/NumberConversionHelper.cs
@@ -2,6 +2,7 @@ namespace DateConverter.Helpers
 {
     public static class NumberConversionHelper
     {
+        public static string ToNepaliNumber(this int? number) => number?.ToString().ToNepaliNumber() ?? "";
         public static string ToNepaliNumber(this int number) => number.ToString().ToNepaliNumber();
         internal static string ToNepaliNumber(this string number) =>  number.Replace("0", "०").Replace("1", "१").Replace("2", "२").Replace("3", "३").Replace("4", "४").Replace("5", "५").Replace("6", "६").Replace("7", "७").Replace("8", "८").Replace("9", "९");
     }
