48443a70438a5c0400a6d76e381b4c7edb018b4e
Gaurav Nyaupane <mail.gauravnyaupane@gmail.com>
2023-03-06 14:51:19 +0545

    Added yearend feature

--- Extensions/YearExtensions.cs
+++ Extensions/YearExtensions.cs
@@ -4,6 +4,7 @@ namespace DateConverter.Extensions
 {
     public static class YearExtensions
     {
+        public static NepaliDate GetYearEnd(this NepaliDate date) => YearUtils.GetYearEnd(date.Year);
         public static List<List<List<int?>>> GetYearCalender(this NepaliDate date) => YearUtils.GetYearCalender(date.Year);
     }
 }
--- Utils/YearUtils.cs
+++ Utils/YearUtils.cs
@@ -2,6 +2,8 @@ namespace DateConverter.Utils
 {
     public abstract class YearUtils
     {
+        public static NepaliDate GetYearEnd(int year) => new NepaliDate(year, 12, DateData.DaysInMonthsForBsYear[year][11]);
+
         public static List<List<List<int?>>> GetYearCalender(int year)
         {
             var yearArray = new List<List<List<int?>>>();
